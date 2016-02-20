87451141e77c9392e0e65a22e4290ccfaa9a0b59
Otso <otso.sorvettula@aalto.fi>
2016-02-20 20:00:12 +0200

    update button controller

--- Arduino/Master/Master.ino
+++ Arduino/Master/Master.ino
@@ -1,12 +1,15 @@
 #include <SoftwareSerial.h>
 SoftwareSerial softSerial(10, 11); // RX, TX
-const int amountOfButtons = 2;
+
+const int amountOfButtons = 5;
 unsigned long time;
 unsigned long nextPressTime[amountOfButtons];
 unsigned long duration = 200;
 
-char solenoidMessages[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
-char motorMessages[] = {'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'};
+char solenoid1Messages[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+char solenoid2Messages[] = {'G', 'H', 'I', 'J', 'K', 'L'};
+char motor1Messages[] = {'M', 'N', 'O', 'P', 'Q'};
+char motor2Messages[] = {'R', 'S', 'T', 'U', 'V'};
 
 int click = 2;
 int clickTime = 100;
@@ -16,9 +19,18 @@ int tempoPin = A0;
 int tempo = 0;
 int lastTempo = 0;
 
+int solenoid1Button = 3;
+int solenoid2Button = 4;
+int motor1Button = 5;
+int motor2Button = 6;
+int resetButton = 7;
+
 void setup() {
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
+  pinMode(solenoid1Button, INPUT_PULLUP);
+  pinMode(solenoid2Button, INPUT_PULLUP);
+  pinMode(motor1Button, INPUT_PULLUP);
+  pinMode(motor2Button, INPUT_PULLUP);
+  pinMode(resetButton, INPUT_PULLUP);
   pinMode(click, OUTPUT);
   
   Serial.begin(9600);
@@ -31,18 +43,27 @@ void setup() {
 
 void loop() {
   time = millis();
-
-  if (digitalRead(7) == LOW && nextPressTime[0] < time) {
-    sendSolenoidMessage();
-  }
-
-  if (digitalRead(8) == LOW && nextPressTime[1] < time) {
-    sendMotorMessage();
+  
+  if (digitalRead(solenoid1Button) == LOW && nextPressTime[0] < time) {
+    sendCtrlMessage(solenoid1Messages[random(sizeof(solenoid1Messages))], 0);
   }
   
-  if (time - lastClick > clickTime) {
-    digitalWrite(click, LOW);
+  if (digitalRead(solenoid2Button) == LOW && nextPressTime[1] < time) {
+    sendCtrlMessage(solenoid2Messages[random(sizeof(solenoid2Messages))], 1);
   }
+  
+  if (digitalRead(motor1Button) == LOW && nextPressTime[2] < time) {
+    sendCtrlMessage(motor1Messages[random(sizeof(motor1Messages))], 2);
+  }
+  
+  if (digitalRead(motor2Button) == LOW && nextPressTime[3] < time) {
+    sendCtrlMessage(motor2Messages[random(sizeof(motor2Messages))], 3);
+  }
+  
+  if (digitalRead(resetButton) == LOW && nextPressTime[4] < time) {
+    Serial.print('W');
+    nextPressTime[4] = time + duration;
+  }A
 
   while (Serial.available()) {
     char value = Serial.read();
@@ -53,6 +74,10 @@ void loop() {
     softSerial.write(value);
   }
   
+  if (time - lastClick > clickTime) {
+    digitalWrite(click, LOW);
+  }
+  
   tempo = (int) map(analogRead(tempoPin), 0, 1023, 0, 9);
   if (tempo != lastTempo) {
     Serial.print(tempo);
@@ -60,16 +85,8 @@ void loop() {
   }  
 }
 
-void sendSolenoidMessage() {
-  char x = solenoidMessages[random(sizeof(solenoidMessages))];
+void sendCtrlMessage(char x, int buttonIndex) {
   Serial.print(x);
   softSerial.write(x);
-  nextPressTime[0] = time + duration;
-}
-
-void sendMotorMessage() {
-  char y = motorMessages[random(sizeof(motorMessages))];
-  Serial.print(y);
-  softSerial.write(y);
-  nextPressTime[1] = time + duration;
+  nextPressTime[buttonIndex] = time + duration;
 }
